73d61bbcf71d5cbd4e37e62df958c68a3079a46c
Realxz1 <ventresco.sugianto@kingsway.school.nz>
2024-09-18 10:25:11 +1200

    Add files via upload
    my first python program

---
+++ welcome.py
@@ -0,0 +1,11 @@
+# My first Pyhone program
+# Author: Ventresco Sugianto
+# Date: 2024-09-18
+# Version: 1.0
+
+
+# This is a comment. It starts with a #.
+# Print out a welcome message.
+print('Hello\n World!') # \n creates a line break
+print() # This creates a line break
+print("Congrats")
