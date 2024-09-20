e75876bcb53028dd12efa5d41543cf01512896fa
Realxz1 <ventresco.sugianto@kingsway.school.nz>
2024-09-20 14:36:20 +1200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Showing how variables work
+# Author: Ventreso Sugianto
+# Date: 20 September 2024
+# Version: 1
+
+
+# Different types of variables
+# Store a number
+num_1 = 69 # assigning 69 to num_1
+print(num_1) # This displays what num_1 contains
+
+# Store a string
+name = "Ventresco"
+print(name)
+name_1 = "What the"
+print(name)
+
+# Do calculations in a variable and store the resut
+sum = 5 + 5 # follows PEP* Python conventions
+print(sum)
+sum_string = "5 + 5 = 10" # this is a string
+print(sum)
+
+
+# Adding strings together is called concatenation
+concatenation = name_1 + sum_string
+print(concatenation)
