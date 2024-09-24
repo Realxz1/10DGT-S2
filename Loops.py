644f817f48bdfb835ffff3af809367c4322818ab
Realxz1 <ventresco.sugianto@kingsway.school.nz>
2024-09-25 09:58:55 +1200

    Add files via upload
    First version of loops

---
+++ Loops.py
@@ -0,0 +1,21 @@
+# Loops and indent
+# Author: Ventresco Sugianto
+# Date 20 September 2024
+# Version: 1
+
+
+# for loops. Repeat for a set number of times
+# for start the loop
+# next is the name of the loop
+# in range tells the loop how many times to run
+# the number in the () is how many times it repeats
+for name_of_loop in range(5):
+    print("ha ")
+
+# while loop. Runs untill a condition is met
+keep_going = "" # empty variable
+while keep_going == "":
+    print("Looping")
+    print("Still looping")
+
+    keep_going = input("Again? Or press any other key to quit")
