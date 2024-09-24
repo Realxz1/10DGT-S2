435227dc7262dc42f83826bbbf8b4cf6a2afe9a7
Realxz1 <ventresco.sugianto@kingsway.school.nz>
2024-09-25 10:21:37 +1200

    Add files via upload
    version 2 of loops. Added user input

--- Loops.py
+++ Loops.py
@@ -1,8 +1,23 @@
 # Loops and indent
 # Author: Ventresco Sugianto
-# Date 20 September 2024
-# Version: 1
+# Date 25 September 2024
+# Version: 2
+# TODO
+    # Get user input (ask the user for their name)
+    # Ask the user for two numbers
+    # Add the numbers together
 
+# Ask the user for their name
+name = input("What is your name?")
+print(f"Kia ora {name}.") # f stands for format. We are formatting the print statement
+
+# Ask the user for two numbers
+num_1 = int(input("What is your favourite number?"))
+num_2 = int(input("What is your least favourite number?"))
+
+# Add numbers together
+sum = num_1 + num_2
+print(f"The numbers added together equal to {sum}")
 
 # for loops. Repeat for a set number of times
 # for start the loop
@@ -10,7 +25,7 @@
 # in range tells the loop how many times to run
 # the number in the () is how many times it repeats
 for name_of_loop in range(5):
-    print("ha ")
+    print("ha!")
 
 # while loop. Runs untill a condition is met
 keep_going = "" # empty variable
